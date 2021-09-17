5afcbdfff526ad66f67f610c4f74c7c94ff1f748
Kapitov Pavel <33999966+molel@users.noreply.github.com>
2021-09-17 18:23:00 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -2,19 +2,19 @@ import socket
 
 sock = socket.socket()
 sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+print('Начало прослушивания порта 9090')
 
-msg = ''
+sock.listen(1)
+conn, addr = sock.accept()
+print('Соединение подключено:', *addr)
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
+    data = conn.recv(1024)
+    if not data:
+        break
+    print('Получено: ', data.decode('utf-8'))
+    conn.send(data)
+    print('Отправлено: ', data.decode('utf-8'))
 
 conn.close()
+print('Соединение отключено')
