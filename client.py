0eabc3cad61c75a8605a88421ba5d0a24e9f85b1
Anastasia Shadynina <70855182+AnastasiaShad@users.noreply.github.com>
2021-09-17 19:27:08 +0300

    new files

--- client.py
+++ client.py
@@ -1,16 +1,15 @@
 import socket
-from time import sleep
-
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect(('localhost', 9090))
+print("Произошло подключение клиента")
+while True:
+    msg = input()
+    sock.send(msg.encode("utf-8"))
+    if msg == 'exit' or msg == '':
+        sock.close()
+        break
+    print(f'Отправка данных серверу {msg}')
+    data = sock.recv(1024)
+    print(f"Получение данных от сервера: {data.decode('utf-8')}")
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
-sock.close()
-
-print(data.decode())
+print('Соединение разорвано')
