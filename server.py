e72e752019310558067f6ec5d4ba2879bbb4e27b
Nikita <n1ktrnv@gmail.com>
2021-09-20 21:09:44 +0300

    Добавляет логи для сервера

--- server.py
+++ server.py
@@ -7,17 +7,25 @@ SERVER_ADDRESS = ('', 9090)
 def _main():
     sock = socket.socket()
     sock.bind(SERVER_ADDRESS)
+    print('Запуск сервера')
     sock.listen()
-    conn, address = sock.accept()
+    print('Начало прослушивания порта', SERVER_ADDRESS[0])
+    conn, addr = sock.accept()
+    print('Подключение клиента', addr)
 
     while True:
         data = conn.recv(1024)
         if not data:
             break
+        decoded = data.decode()
+        print(f'Прием данных "{decoded}" от клиента', addr)
         conn.send(data)
+        print(f'Отправка данных "{decoded}" клиенту', addr)
 
     conn.close()
+    print('Отключание клиента', addr)
     sock.close()
+    print('Отключение сервера')
 
 
 if __name__ == '__main__':
