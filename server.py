6a3fb95102d3c95532161e268e4c62d120db62fd
wed.addams <alenkapyh1@gmail.com>
2021-09-24 20:37:18 +0300

    commit ( 'Создала сервер')

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 import socket
 
 sock = socket.socket()
+print('Запуск сервера')
 sock.bind(('', 9090))
-sock.listen(0)
+print('Начало прослушивания порта')
+sock.listen(1)
 conn, addr = sock.accept()
+print('Подключение клиента')
 print(addr)
+print('Соединение подключено')
 
 msg = ''
 
@@ -12,9 +16,12 @@ while True:
 	data = conn.recv(1024)
 	if not data:
 		break
+	print('Прием данных от клиента', data.decode())
 	msg += data.decode()
 	conn.send(data)
+	print('Отправка данных клиенту', data.decode())
 
 print(msg)
-
+print('Остановка клиента')
 conn.close()
+print('Остановка сервера')
