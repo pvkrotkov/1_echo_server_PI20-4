faaa00f4d28ed450923a120db2bff359e604d073
HanyBin <51113403+HanyBin@users.noreply.github.com>
2021-09-24 17:34:19 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -3,14 +3,15 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect(('127.0.0.1', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+msg = input()
+while msg != 'exit':
+    sock.send(msg.encode())
+    data = sock.recv(1024)
+    print(data.decode())
+    msg = input()
+else:
+    sock.close()
 
-data = sock.recv(1024)
 
-sock.close()
-
-print(data.decode())
