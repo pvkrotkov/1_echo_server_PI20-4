35a292c286a7b54e76fdbe55785c9d937ec0f2c0
AlexaErk <70998859+AlexaPI20-4@users.noreply.github.com>
2021-09-24 18:18:02 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,20 +1,21 @@
 import socket
 
 sock = socket.socket()
+print('Запускаем сервер')
 sock.bind(('', 9090))
-sock.listen(0)
+print('Начинаем прослушивание порта 9090')
+sock.listen(1)
 conn, addr = sock.accept()
-print(addr)
-
-msg = ''
+print('Соединение налажено')
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    data = conn.recv(1024)
+    if not data:
+        print('Клиент отключен')
+        break
+    print('От клиента получено: ', data.decode('utf-8'))
+    conn.send(data)
+    print('Клиенту отправлено: ', data.decode('utf-8'))
 
-print(msg)
-
+print('Остановка сервера')
 conn.close()
