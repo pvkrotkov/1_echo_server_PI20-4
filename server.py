0f0168b4f9a010579416e17b7de10c2b13c8b182
aeSTet <76133815+aeSTet@users.noreply.github.com>
2021-09-24 17:59:27 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,26 @@
 import socket
 
-sock = socket.socket()
-sock.bind(('127.0.0.1', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+SERVER_ADDRESS = ('127.0.0.1', 9090)
+
+
+sock = socket.socket()
+sock.bind(SERVER_ADDRESS)
+print('Запуск сервера', SERVER_ADDRESS)
+sock.listen()
+print('Начало прослушивания порта', SERVER_ADDRESS[1])
+conn, addr = sock.accept()
+print('Подключение клиента', addr)
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
+    data = conn.recv(1024)
+    if not data:
+        break
+    print(f'Прием данных {data.decode()} от клиента', addr)
+    conn.send(data)
+    print(f'Отправка данных {data.decode()} клиенту', addr)
+print('Отключение клиента', addr)
 
 conn.close()
+sock.close()
+print('Остановка сервера', SERVER_ADDRESS)
