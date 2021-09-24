fe21023f0cb2f4963bf407799300ebdc7fb91e57
wed.addams <alenkapyh1@gmail.com>
2021-09-24 20:41:33 +0300

    commit ('Создала клиент')

--- client.py
+++ client.py
@@ -2,15 +2,16 @@ import socket
 from time import sleep
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
-
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
+sock.connect(('localhost', 9090))
+print('Произошло сединение с сервером')
+while True:
+    msg = str(input('Введите строку или выход" чтобы выйти: '))
+    sock.send(msg.encode())
+    if msg == 'выход':
+        break
+    print(f'Отправка данных серверу {msg}')
+    data = sock.recv(1024)
+    print(f'Прием данных сервером: {data.decode()}')
 
 sock.close()
-
-print(data.decode())
+print('Разрыв соединения с сервером')
