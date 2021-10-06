1ab2a0c5eddea69fc8931c36e87a88a752d4a516
Юля Нагубнева <gubiky@mail.ru>
2021-10-07 01:33:47 +0300

    Добавляет комментарии к UDP  серверу.

--- server_udp.py
+++ server_udp.py
@@ -4,9 +4,9 @@ import socket
 SERVER_ADDRESS = ('', 9090)
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(SERVER_ADDRESS)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #  Создание объекта socket, указание его типа - UDP.
+sock.bind(SERVER_ADDRESS) #  Cвязывает адрес и порт с сокетом.
 
 while True:
-    data, addr = sock.recvfrom(1024)
+    data, addr = sock.recvfrom(1024) #  Возвращает данные от клиента размером 1 кБ и адрес клиента.
     print(f'Получено сообщение: {data.decode()} от клиента', addr)
