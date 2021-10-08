9a6e1c0b0b406a617affd8f62c22ebc48924f1ea
Notmall <70998909+Notmall@users.noreply.github.com>
2021-10-08 18:41:32 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,21 @@
 import socket
-from time import sleep
-
+SERVER_ADDRESS = ('127.0.0.1', 9090)
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.bind(SERVER_ADDRESS)
+print('Запуск сервера', SERVER_ADDRESS)
+sock.listen()
+print('Начало прослушивания порта', SERVER_ADDRESS[1])
+conn, addr = sock.accept()
+print('Подключение клиента', addr)
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    print(f'Прием данных {data.decode()} от клиента', addr)
+    conn.send(data)
+    print(f'Отправка данных {data.decode()} клиенту', addr)
+print('Отключение клиента', addr)
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
+conn.close()
 sock.close()
-
-print(data.decode())
+print('Остановка сервера', SERVER_ADDRESS)
