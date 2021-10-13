c1274e02a6515e2fd8836af586082dcc9ad01100
Madina Sanakoeva <70848413+Madina-2001@users.noreply.github.com>
2021-10-14 01:14:11 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,26 @@
 import socket
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+def ser_ver():
+    sock = socket.socket() #Создаём сокет
+    sock.bind(('', 9090))   #Берём сокет с хостом и портом и связываем сокет с данным хостом и портом
+    print('Запуск сервера')
+    sock.listen() #Запускаем прослушивание
+    print('Начало прослушивания порта', 9090)
+    conn, addr = sock.accept()   #Приём подключения клиента к серверу
+    print('Подключение клиента', addr)
 
-msg = ''
+    while True:               #Производим считывание данных клиента небольшими порциями
+        data = conn.recv(1024)
+        if not data:              #Производим отправку данных клиенту
+            break
+        dec = data.decode()
+        print(f'Прием данных "{dec}" от клиента', addr)
+        conn.send(data)
+        print(f'Отправка данных "{dec}" клиенту', addr)
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
+    conn.close()
+    print('Отключение клиента', addr) #Производим отключение клиента и остановку сервера
+    sock.close()
+    print('Отключение сервера')
+    
+ser_ver()
