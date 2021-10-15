1127ca23f1a2a08ddf9d1b7e30c44d1c192945f2
1 <2>
2021-10-15 18:34:45 +0300

    echo server

--- server.py
+++ server.py
@@ -1,20 +1,22 @@
 import socket
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+def master():
+    sock = socket.socket()
+    sock.bind(('', 9090))
+    sock.listen()
+    conn, msg = sock.accept()
+    print('Подключение usera', msg)
 
-msg = ''
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print(f'Прием данных от user: {data.decode()}', msg)
+        conn.send(data)
+        print(f'Отправка данных к user: {data.decode()}', msg)
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    conn.close()
+    sock.close()
+    print('ОТКЛЮЧЕНИЕ')
 
-print(msg)
-
-conn.close()
+master()
