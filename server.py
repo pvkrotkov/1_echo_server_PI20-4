70779ef7912bc44f3ee0f4dd5f0bc96cdd42c9ab
Zevety <90498783+Zevety@users.noreply.github.com>
2021-11-11 20:46:35 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,44 @@
+import sys
+import time
 import socket
 
 sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+try:
+    sock.bind(('localhost', 9090))
+    print('Сервер запущен')
+    try:
+        sock.listen(1)
+        print('Прослушивание парта запущено')
+        try:
+            client, address = sock.accept()
+            print('Подключение клиента')
+            while True:
+                text = ''
+                try:
+                    data = client.recv(1024)
+                    print('Получение данных от клиента')
+                    if not data:
+                        break
+                    text += data.decode()
+                    try:
+                        client.send(data)
+                        print('Получено сообщение: ' + text)
+                        print('Отправка данных клиенту')
+                        if text == 'exit':
+                            break
+                            client.close()
+                    except:
+                        print('Ошибка при отправке данных клиенту')
+                except:
+                    print('Ошибка при получении данных от клиента')
+                    break
+        except:
+            print('Ошибка при подключении клиента')
+    except:
+        print('Ошибка при прослушивании порта')
+except:
+    print('Ошибка запуска сервера')
 
-msg = ''
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
+print('Клиент отключен')
+print('Сервер остановлен')
